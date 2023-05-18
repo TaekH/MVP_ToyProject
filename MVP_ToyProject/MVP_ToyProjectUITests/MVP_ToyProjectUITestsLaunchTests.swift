cdf9f120c7d7c37e2fda422ede72ba1f972bfe1d
Taekhwan Han <hth3396@gmail.com>
2023-05-18 16:15:15 +0900

    init project

---
+++ MVP_ToyProject/MVP_ToyProjectUITests/MVP_ToyProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVP_ToyProjectUITestsLaunchTests.swift
+//  MVP_ToyProjectUITests
+//
+//  Created by 한택환 on 2023/05/18.
+//
+
+import XCTest
+
+final class MVP_ToyProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
