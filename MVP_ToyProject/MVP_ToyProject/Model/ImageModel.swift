512de14380783178215dc4bb45da5e3d7fd2dd96
Taekhwan Han <hth3396@gmail.com>
2023-06-25 15:04:36 +0900

    init projectdirectory

---
+++ MVP_ToyProject/MVP_ToyProject/Model/ImageModel.swift
@@ -0,0 +1,12 @@
+//
+//  ImageModel.swift
+//  MVP_ToyProject
+//
+//  Created by 한택환 on 2023/06/25.
+//
+
+import Foundation
+
+struct ImageModel: Decodable {
+    
+}
---
+++ MVP_ToyProject/MVP_ToyProject/Services/NetworkService.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkService.swift
+//  MVP_ToyProject
+//
+//  Created by 한택환 on 2023/06/25.
+//
+
+import Foundation
+
+final class NetworkService {
+    
+}
